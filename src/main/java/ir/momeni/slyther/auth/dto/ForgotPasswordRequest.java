20fda2ed02b801fa3d404be28aa8e3b5d3afa849
lilUnkelDemon <amir.momeni.gelloria79@gmail.com>
2025-11-11 00:35:19 +0330

    docs: clarify fields in ForgotPasswordRequest DTO

--- src/main/java/ir/momeni/slyther/auth/dto/ForgotPasswordRequest.java
+++ src/main/java/ir/momeni/slyther/auth/dto/ForgotPasswordRequest.java
@@ -1,4 +1,21 @@
 package ir.momeni.slyther.auth.dto;
+
 import jakarta.validation.constraints.NotBlank;
-import lombok.Getter; @Getter
-public class ForgotPasswordRequest { @NotBlank private String email; }
+import lombok.Getter;
+
+
+/**
+ * Request DTO for initiating a "forgot password" flow.
+ * <p>
+ * Contains the registered email address that will receive a password reset token.
+ */
+
+@Getter
+public class ForgotPasswordRequest {
+
+    /**
+     * Email of the user requesting password reset. Must not be blank.
+     */
+    @NotBlank
+    private String email;
+}
